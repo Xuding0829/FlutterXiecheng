ba8e9f6a2508c30e067f7d92112338632b4f5a14
Xuding0829 <2373700847@qq.com>
2024-09-03 14:01:43 +0800

    first commit

---
+++ lib/navigator/tab_navigator.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class TabNavigator extends StatefulWidget {
+  const TabNavigator({Key? key}) : super(key: key);
+  @override
+  _TabNavigatorState createState() => _TabNavigatorState();
+}
+
+class _TabNavigatorState extends State<TabNavigator> {}
